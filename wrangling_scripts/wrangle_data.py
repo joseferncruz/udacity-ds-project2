1283862956b89cf78131f177c745412b11289f1c
joseferncruz <29801616+joseferncruz@users.noreply.github.com>
2021-05-12 11:15:32 -0400

    modify title of ind figures

--- wrangling_scripts/wrangle_data.py
+++ wrangling_scripts/wrangle_data.py
@@ -12,7 +12,7 @@ import json
 # Constants
 DATA = pd.read_csv('data/noise_data_clean.csv', index_col=0)
 
-    
+
 def return_figures():
     """Create 4 plotly visualizations for dashboard.
 
@@ -27,15 +27,15 @@ def return_figures():
 
     # FIGURE 1
     fig1_data = get_fig_one_data(DATA)
-    
-    graph_one = []    
+
+    graph_one = []
     graph_one.append(
       go.Scatter(x=fig1_data.x,
                  y=fig1_data.y_2019,
                  mode='lines',
                  name="2019")
     )
-    
+
     graph_one.append(
       go.Scatter(x=fig1_data.x,
                  y=fig1_data.y_2020,
@@ -51,16 +51,16 @@ def return_figures():
     layout_one = dict(title='<b>Noise complaints between January 2019 and April 2021<b>',
                       showlegend=True,
                       yaxis=dict(title='number of complaints'),)
-    
-    # FIGURE 2 
+
+    # FIGURE 2
     fig2_data = get_fig_two_data(DATA)
-    
+
     graph_two = []
 
     graph_two.append(
       go.Bar(x=fig2_data.x,
              y=fig2_data.y_2019,
-             name='2019'), 
+             name='2019'),
     )
     graph_two.append(
       go.Bar(x=fig2_data.x,
@@ -68,7 +68,7 @@ def return_figures():
              name='2020'),
     )
 
-    layout_two = dict(title='<b>Noise complaints in each NYC borough<b>',
+    layout_two = dict(title='<b>Noise complaints by borough<b>',
                       barmode='group',
                       yaxis=dict(title='number of complaints'),)
 
@@ -76,7 +76,7 @@ def return_figures():
     fig3_data = get_fig_three_data(DATA)
 
     graph_three = []
-    
+
     graph_three.append(
       go.Bar(x=fig3_data.x,
              y=fig3_data.y_2019,
@@ -87,16 +87,16 @@ def return_figures():
              y=fig3_data.y_2020,
              name='2020',)
     )
-    layout_three = dict(title='<b> Top 3 noise complaints<b>',
+    layout_three = dict(title='<b>Top 3 types of complaints<b>',
                         barmode='group',
                         xaxis=dict(title='type of complaint'),
                         yaxis = dict(title='number of complaints'),)
-    
+
 # FIGURE 4
     fig1_data = get_fig_four_data(DATA)
-    
+
     graph_four = []
-    
+
     for complaint_type in fig1_data.keys():
 
         graph_four.append(
@@ -105,12 +105,12 @@ def return_figures():
                  name=f'{complaint_type}',
                  orientation='h',)
         )
-    
-    layout_four = dict(title='<b>Type of noise complaints during the weekdays<b>',
+
+    layout_four = dict(title='<b>Daily types of complaints<b>',
                         barmode='stack',
-                        xaxis=dict(title='percentage of total complaints (%)'),)
-                
-    
+                        xaxis=dict(title='percentage of total complaints'),)
+
+
     # Append all charts to the figures list
     figures = []
     figures.append(dict(data=graph_one, layout=layout_one))
